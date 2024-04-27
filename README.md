acdb0101c5dc798442d774bb9145d9df2c176271
Maciej Zielinski <32851056+virtuallynowhere@users.noreply.github.com>
2024-04-27 12:17:08 +0100

    Initial files for MakeCode project

---
+++ README.md
@@ -0,0 +1,24 @@
+
+> Open this page at [https://virtuallynowhere.github.io/guess_the_number/](https://virtuallynowhere.github.io/guess_the_number/)
+
+## Use as Extension
+
+This repository can be added as an **extension** in MakeCode.
+
+* open [https://makecode.microbit.org/](https://makecode.microbit.org/)
+* click on **New Project**
+* click on **Extensions** under the gearwheel menu
+* search for **https://github.com/virtuallynowhere/guess_the_number** and import
+
+## Edit this project
+
+To edit this repository in MakeCode.
+
+* open [https://makecode.microbit.org/](https://makecode.microbit.org/)
+* click on **Import** then click on **Import URL**
+* paste **https://github.com/virtuallynowhere/guess_the_number** and click import
+
+#### Metadata (used for search, rendering)
+
+* for PXT/microbit
+<script src="https://makecode.com/gh-pages-embed.js"></script><script>makeCodeRender("{{ site.makecode.home_url }}", "{{ site.github.owner_name }}/{{ site.github.repository_name }}");</script>
---
+++ main.ts
@@ -0,0 +1,65 @@
+function checkGuess (guess: boolean) {
+    nextNumber = Math.randomRange(1, 9)
+    if (guess == nextNumber >= currentNumber) {
+        score += 1
+        basic.showIcon(IconNames.Happy)
+    } else {
+        basic.showIcon(IconNames.Sad)
+    }
+    turn += 1
+    currentNumber = nextNumber
+    basic.pause(500)
+}
+function howLong () {
+    rounds = 3
+    basic.showString("?")
+    while (!(input.logoIsPressed())) {
+        if (input.buttonIsPressed(Button.A)) {
+            rounds += -2
+            basic.showNumber(rounds)
+        } else if (input.buttonIsPressed(Button.B)) {
+            rounds += 2
+            basic.showNumber(rounds)
+        }
+    }
+    if (rounds < 0) {
+        music.play(music.builtinPlayableSoundEffect(soundExpression.sad), music.PlaybackMode.InBackground)
+        basic.showIcon(IconNames.Chessboard)
+        howLong()
+    } else {
+        turn = 1
+        score = 0
+        playGame()
+    }
+}
+function playGame () {
+    currentNumber = Math.randomRange(1, 9)
+    while (turn <= rounds) {
+        basic.showNumber(currentNumber)
+        if (input.buttonIsPressed(Button.A)) {
+            checkGuess(false)
+        } else if (input.buttonIsPressed(Button.B)) {
+            checkGuess(true)
+        }
+    }
+    gameOver()
+}
+function gameOver () {
+    basic.clearScreen()
+    if (score >= rounds / 2) {
+        music.play(music.builtinPlayableSoundEffect(soundExpression.happy), music.PlaybackMode.InBackground)
+        basic.showString("Winner")
+    } else {
+        music.play(music.builtinPlayableSoundEffect(soundExpression.slide), music.PlaybackMode.InBackground)
+        basic.showString("Loser")
+    }
+    basic.pause(500)
+    howLong()
+}
+let rounds = 0
+let turn = 0
+let score = 0
+let currentNumber = 0
+let nextNumber = 0
+basic.clearScreen()
+howLong()
